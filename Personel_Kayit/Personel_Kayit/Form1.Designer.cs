cc51975267e3dd110db59a8e01a0a3abb15c713a
Talha Pakdil <talhapakdil7@gmail.com>
2025-02-26 17:44:45 +0300

    first commit

---
+++ Personel_Kayit/Personel_Kayit/Form1.Designer.cs
@@ -0,0 +1,373 @@
+﻿namespace Personel_Kayit
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            txtid = new TextBox();
+            txtsoyad = new TextBox();
+            txtad = new TextBox();
+            comboBox1 = new ComboBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            label6 = new Label();
+            groupBox2 = new GroupBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            groupBox3 = new GroupBox();
+            dataGridView1 = new DataGridView();
+            maskedTextBox1 = new MaskedTextBox();
+            label7 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(maskedTextBox1);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(txtad);
+            groupBox1.Controls.Add(txtsoyad);
+            groupBox1.Controls.Add(txtid);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(543, 398);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Personel Kayıt";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(64, 65);
+            label1.Name = "label1";
+            label1.Size = new Size(121, 28);
+            label1.TabIndex = 0;
+            label1.Text = "Personel Id:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(64, 111);
+            label2.Name = "label2";
+            label2.Size = new Size(129, 28);
+            label2.TabIndex = 1;
+            label2.Text = "Personel Ad:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(64, 160);
+            label3.Name = "label3";
+            label3.Size = new Size(77, 28);
+            label3.TabIndex = 2;
+            label3.Text = "Soyad:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(64, 206);
+            label4.Name = "label4";
+            label4.Size = new Size(67, 28);
+            label4.TabIndex = 3;
+            label4.Text = "Şehir:";
+            // 
+            // txtid
+            // 
+            txtid.Location = new Point(201, 65);
+            txtid.Name = "txtid";
+            txtid.Size = new Size(125, 36);
+            txtid.TabIndex = 4;
+            // 
+            // txtsoyad
+            // 
+            txtsoyad.Location = new Point(201, 160);
+            txtsoyad.Name = "txtsoyad";
+            txtsoyad.Size = new Size(125, 36);
+            txtsoyad.TabIndex = 5;
+            // 
+            // txtad
+            // 
+            txtad.Location = new Point(201, 111);
+            txtad.Name = "txtad";
+            txtad.Size = new Size(125, 36);
+            txtad.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(201, 206);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(125, 36);
+            comboBox1.TabIndex = 7;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(201, 253);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(65, 32);
+            radioButton1.TabIndex = 8;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "evli";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(272, 253);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(86, 32);
+            radioButton2.TabIndex = 9;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "bekar";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(64, 253);
+            label5.Name = "label5";
+            label5.Size = new Size(84, 28);
+            label5.TabIndex = 10;
+            label5.Text = "Durum:";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(201, 291);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 36);
+            textBox4.TabIndex = 11;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(64, 299);
+            label6.Name = "label6";
+            label6.Size = new Size(84, 28);
+            label6.TabIndex = 12;
+            label6.Text = "Meslek:";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(button7);
+            groupBox2.Controls.Add(button6);
+            groupBox2.Controls.Add(button5);
+            groupBox2.Controls.Add(button4);
+            groupBox2.Controls.Add(button3);
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(button1);
+            groupBox2.Location = new Point(597, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(249, 398);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "İşlemler";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ButtonFace;
+            button1.Location = new Point(30, 52);
+            button1.Name = "button1";
+            button1.Size = new Size(191, 39);
+            button1.TabIndex = 0;
+            button1.Text = "Listele";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ButtonFace;
+            button2.Location = new Point(30, 97);
+            button2.Name = "button2";
+            button2.Size = new Size(191, 39);
+            button2.TabIndex = 1;
+            button2.Text = "Kaydet";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ButtonFace;
+            button3.Location = new Point(30, 142);
+            button3.Name = "button3";
+            button3.Size = new Size(191, 39);
+            button3.TabIndex = 2;
+            button3.Text = "SİL";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ButtonFace;
+            button4.Location = new Point(30, 187);
+            button4.Name = "button4";
+            button4.Size = new Size(191, 39);
+            button4.TabIndex = 3;
+            button4.Text = "Güncelle";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.ButtonFace;
+            button5.Location = new Point(30, 232);
+            button5.Name = "button5";
+            button5.Size = new Size(191, 39);
+            button5.TabIndex = 4;
+            button5.Text = "Temizle";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = SystemColors.ButtonFace;
+            button6.Location = new Point(30, 277);
+            button6.Name = "button6";
+            button6.Size = new Size(191, 39);
+            button6.TabIndex = 5;
+            button6.Text = "İstatistik";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = SystemColors.ButtonFace;
+            button7.Location = new Point(30, 322);
+            button7.Name = "button7";
+            button7.Size = new Size(191, 39);
+            button7.TabIndex = 6;
+            button7.Text = "Grafikler";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(dataGridView1);
+            groupBox3.Location = new Point(12, 432);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(944, 178);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Kayıtlar";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(3, 32);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(938, 143);
+            dataGridView1.TabIndex = 0;
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(201, 345);
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(125, 36);
+            maskedTextBox1.TabIndex = 13;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(64, 348);
+            label7.Name = "label7";
+            label7.Size = new Size(68, 28);
+            label7.TabIndex = 14;
+            label7.Text = "Maaş:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 28F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightSeaGreen;
+            ClientSize = new Size(968, 622);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Font = new Font("Corbel", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private ComboBox comboBox1;
+        private TextBox txtad;
+        private TextBox txtsoyad;
+        private TextBox txtid;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Label label6;
+        private TextBox textBox4;
+        private Label label5;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private GroupBox groupBox2;
+        private Button button1;
+        private Button button7;
+        private Button button6;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private GroupBox groupBox3;
+        private DataGridView dataGridView1;
+        private Label label7;
+        private MaskedTextBox maskedTextBox1;
+    }
+}
